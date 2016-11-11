1a64cbdd6017b6fcd0e66441f4c2902d4690f5cb
Bertie Croll <bert.croll@gmail.com>
2016-11-11 14:14:45 +0000

    Subject class constructor added

--- app/src/main/java/com/example/user/lessontracker/Subject.java
+++ app/src/main/java/com/example/user/lessontracker/Subject.java
@@ -1,4 +1,10 @@
 package com.example.user.lessontracker;
 
 public class Subject implements Teachable {
+
+    private String mTitle;
+
+    public Subject(String title) {
+        mTitle = title;
+    }
 }
