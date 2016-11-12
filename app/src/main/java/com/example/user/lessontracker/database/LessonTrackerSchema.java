54d0f137e6b3acf050e593732902344ee7bab18f
Bertie Croll <bert.croll@gmail.com>
2016-11-12 16:09:27 +0000

    SubjectTable and TopicTable inner classes created within LessonTrackerSchema

--- app/src/main/java/com/example/user/lessontracker/database/LessonTrackerSchema.java
+++ app/src/main/java/com/example/user/lessontracker/database/LessonTrackerSchema.java
@@ -1,4 +1,13 @@
 package com.example.user.lessontracker.database;
 
 public class LessonTrackerSchema {
+
+    public static final class SubjectTable {
+        public static final String NAME = "subjects";
+    }
+
+    public static final class TopicTable {
+        public static final String NAME = "topics";
+    }
+
 }
