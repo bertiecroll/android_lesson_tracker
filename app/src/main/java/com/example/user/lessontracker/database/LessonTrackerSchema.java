67100d2f98b024b35bf25e81fabb409bea489742
Bertie Croll <bert.croll@gmail.com>
2016-11-12 16:12:40 +0000

    Cols inner class added to both SubjectTable and TopicTable defining column names

--- app/src/main/java/com/example/user/lessontracker/database/LessonTrackerSchema.java
+++ app/src/main/java/com/example/user/lessontracker/database/LessonTrackerSchema.java
@@ -4,10 +4,23 @@ public class LessonTrackerSchema {
 
     public static final class SubjectTable {
         public static final String NAME = "subjects";
+
+        public static final class Cols {
+            public static final String ID = "id";
+            public static final String TITLE = "title";
+            public static final String DETAIL = "detail";
+        }
     }
 
     public static final class TopicTable {
         public static final String NAME = "topics";
+
+        public static final class Cols {
+            public static final String ID = "id";
+            public static final String SUBJECT_ID = "subject_id";
+            public static final String TITLE = "title";
+            public static final String DETAIL = "detail";
+        }
     }
 
 }
