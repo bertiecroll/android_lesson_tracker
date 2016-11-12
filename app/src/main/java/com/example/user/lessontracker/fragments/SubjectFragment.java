1e46b5ff3f37dcdf1d540a9f3152edd9bf5065fe
Bertie Croll <bert.croll@gmail.com>
2016-11-12 22:54:10 +0000

    TextView variables added to SubjectFragment class

--- app/src/main/java/com/example/user/lessontracker/fragments/SubjectFragment.java
+++ app/src/main/java/com/example/user/lessontracker/fragments/SubjectFragment.java
@@ -5,13 +5,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.user.lessontracker.R;
 import com.example.user.lessontracker.database.LessonTrackerDbHelper;
+import com.example.user.lessontracker.models.Subject;
+
+import org.w3c.dom.Text;
 
 public class SubjectFragment extends Fragment {
 
-    private LessonTrackerDbHelper mDbHelper;
+    LessonTrackerDbHelper mDbHelper;
+    TextView mTitleTextView;
+    TextView mDetailTextView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,6 +29,11 @@ public class SubjectFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_subject, container, false);
 
         mDbHelper = new LessonTrackerDbHelper(getActivity());
+        mTitleTextView = (TextView) view.findViewById(R.id.subject_title);
+        mDetailTextView = (TextView) view.findViewById(R.id.subject_detail);
+
+        Subject subject = new Subject(mTitleTextView.getText().toString(), mDetailTextView.getText().toString());
+        subject.save(mDbHelper);
 
         return view;
     }
