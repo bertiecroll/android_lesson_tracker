a3e12d7d73fd2fdee18e88c13f20ed22c1120668
Bertie Croll <bert.croll@gmail.com>
2016-11-12 19:34:02 +0000

    basic onCreate and onCreateView methods added to SubjectFragment

--- app/src/main/java/com/example/user/lessontracker/fragments/SubjectFragment.java
+++ app/src/main/java/com/example/user/lessontracker/fragments/SubjectFragment.java
@@ -1,6 +1,24 @@
 package com.example.user.lessontracker.fragments;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.user.lessontracker.R;
 
 public class SubjectFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_subject, container, false);
+        return view;
+    }
+
 }
