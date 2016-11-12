ead7e3d5d24a13f341fc39a5e9e4bde186b14dec
Bertie Croll <bert.croll@gmail.com>
2016-11-12 20:46:15 +0000

    FragmentManager added to onCreate method in SubjectActivity class

--- app/src/main/java/com/example/user/lessontracker/activities/SubjectActivity.java
+++ app/src/main/java/com/example/user/lessontracker/activities/SubjectActivity.java
@@ -1,9 +1,12 @@
 package com.example.user.lessontracker.activities;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 
 import com.example.user.lessontracker.R;
+import com.example.user.lessontracker.fragments.SubjectFragment;
 
 public class SubjectActivity extends FragmentActivity {
 
@@ -11,5 +14,13 @@ public class SubjectActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subject);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new SubjectFragment();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
     }
 }
