c4e510e41a9d827621149881961d76756eae6f46
Bertie Croll <bert.croll@gmail.com>
2016-11-12 17:56:45 +0000

    layout directory created and activity_subject.xml added

--- app/src/main/java/com/example/user/lessontracker/activities/SubjectActivity.java
+++ app/src/main/java/com/example/user/lessontracker/activities/SubjectActivity.java
@@ -1,6 +1,8 @@
 package com.example.user.lessontracker.activities;
 
+import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
 public class SubjectActivity extends FragmentActivity {
+
 }
