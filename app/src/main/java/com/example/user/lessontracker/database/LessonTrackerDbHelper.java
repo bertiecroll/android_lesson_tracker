25a00b1549a2b13988edbc952bd192db431b41f6
Bertie Croll <bert.croll@gmail.com>
2016-11-12 16:20:30 +0000

    constructor added to LessonTrackerDbHelper

--- app/src/main/java/com/example/user/lessontracker/database/LessonTrackerDbHelper.java
+++ app/src/main/java/com/example/user/lessontracker/database/LessonTrackerDbHelper.java
@@ -1,7 +1,24 @@
 package com.example.user.lessontracker.database;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class LessonTrackerDbHelper extends SQLiteOpenHelper {
+    private static final int VERSION = 1;
+    private static final String DATABASE_NAME = "lessonTracker.db";
 
+    public LessonTrackerDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
