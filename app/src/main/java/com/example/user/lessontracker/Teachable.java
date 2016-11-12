b9de412e5454b77c795f1dba4051fd64308c0208
Bertie Croll <bert.croll@gmail.com>
2016-11-12 16:37:38 +0000

    Teachable interface amended

--- app/src/main/java/com/example/user/lessontracker/Teachable.java
+++ app/src/main/java/com/example/user/lessontracker/Teachable.java
@@ -1,6 +1,10 @@
 package com.example.user.lessontracker;
 
 public interface Teachable {
+    public int getId();
+    public void setId(int id);
     public String getTitle();
     public void setTitle(String newTitle);
+    public String getDetail();
+    public void setDetail(String newDetail);
 }
