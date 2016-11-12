34e2baf62091bee72873ef3b3a29df57a136455d
Bertie Croll <bert.croll@gmail.com>
2016-11-12 18:08:40 +0000

    onCreate method added to SubjectActivity

--- app/src/main/java/com/example/user/lessontracker/activities/SubjectActivity.java
+++ app/src/main/java/com/example/user/lessontracker/activities/SubjectActivity.java
@@ -3,6 +3,13 @@ package com.example.user.lessontracker.activities;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
+import com.example.user.lessontracker.R;
+
 public class SubjectActivity extends FragmentActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subject);
+    }
 }
