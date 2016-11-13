842390e9c28bdd6466c6427514e85edb0372f50c
Bertie Croll <bert.croll@gmail.com>
2016-11-13 15:31:26 +0000

    Button added to AddTopicFragment

--- app/src/main/java/com/example/user/lessontracker/fragments/AddTopicFragment.java
+++ app/src/main/java/com/example/user/lessontracker/fragments/AddTopicFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -15,6 +16,7 @@ public class AddTopicFragment extends Fragment {
     TextView mSubjectText;
     EditText mTitleEditText;
     EditText mDetailEditText;
+    Button mAddButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,6 +26,13 @@ public class AddTopicFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_add_topic, container, false);
+
+        mSubjectText = (TextView) view.findViewById(R.id.topic_subject_text);
+        mTitleEditText = (EditText) view.findViewById(R.id.topic_title_edit);
+        mDetailEditText = (EditText) view.findViewById(R.id.topic_detil_edit);
+        mAddButton = (Button) view.findViewById(R.id.topic_add_button);
+
+
 
         return view;
     }
