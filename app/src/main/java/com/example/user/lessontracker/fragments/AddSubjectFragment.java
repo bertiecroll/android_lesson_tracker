8cedc82ba0ebbc70df4a04e2874d9663e2fc3d30
Bertie Croll <bert.croll@gmail.com>
2016-11-13 11:23:57 +0000

    fragment_add_subject.xml created with two EditText views

--- app/src/main/java/com/example/user/lessontracker/fragments/AddSubjectFragment.java
+++ app/src/main/java/com/example/user/lessontracker/fragments/AddSubjectFragment.java
@@ -2,6 +2,11 @@ package com.example.user.lessontracker.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.user.lessontracker.R;
 
 /**
  * Created by user on 13/11/2016.
@@ -12,5 +17,14 @@ public class AddSubjectFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_subject, container, false);
+
+
+
+        return view;
     }
 }
