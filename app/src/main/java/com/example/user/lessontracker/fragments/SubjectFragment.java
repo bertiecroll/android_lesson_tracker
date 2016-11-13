7a2235102af96e14f9c4f4ff51e3b184f9b77e0f
Bertie Croll <bert.croll@gmail.com>
2016-11-13 21:57:51 +0000

    ItemClickListener and onItemClick methods added to TopicList

--- app/src/main/java/com/example/user/lessontracker/fragments/SubjectFragment.java
+++ app/src/main/java/com/example/user/lessontracker/fragments/SubjectFragment.java
@@ -3,9 +3,11 @@ package com.example.user.lessontracker.fragments;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -92,6 +94,14 @@ public class SubjectFragment extends Fragment {
                     android.R.layout.simple_list_item_1, topics);
 
             mTopicList.setAdapter(adapter);
+
+            mTopicList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                   Topic selectedTopic = (Topic) mTopicList.getItemAtPosition(position);
+                   Log.d("mTopicList", selectedTopic + " selected");
+                }
+            });
         } else {
             mNewTopicButton.setVisibility(View.INVISIBLE);
             mTopicList.setVisibility(View.INVISIBLE);
