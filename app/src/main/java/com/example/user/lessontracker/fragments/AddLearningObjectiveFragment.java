3892279e9907bc941a515cb68c252bc7c70be871
Bertie Croll <bert.croll@gmail.com>
2016-11-14 10:06:11 +0000

    AddLearningObjectiveFragment created

---
+++ app/src/main/java/com/example/user/lessontracker/fragments/AddLearningObjectiveFragment.java
@@ -0,0 +1,35 @@
+package com.example.user.lessontracker.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.user.lessontracker.R;
+
+public class AddLearningObjectiveFragment extends Fragment {
+
+    TextView mTopicText;
+    EditText mTitleEditText;
+    EditText mDetailEditText;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_add_learning_objective, container, false);
+
+        mTopicText = (TextView) view.findViewById(R.id.learning_objective_topic_text);
+
+        mTitleEditText = (EditText) view.findViewById(R.id.learning_objective_title_edit);
+        mDetailEditText = (EditText) view.findViewById(R.id.learning_objective_detail_edit);
+
+        return view;
+    }
+}
