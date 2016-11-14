b85267bcef4ff94e65f7f977dbf49ac46c0b3f4b
Bertie Croll <bert.croll@gmail.com>
2016-11-14 10:18:55 +0000

    mTopicText updated with Topic title in AddLearningObjectiveFragment

--- app/src/main/java/com/example/user/lessontracker/fragments/AddLearningObjectiveFragment.java
+++ app/src/main/java/com/example/user/lessontracker/fragments/AddLearningObjectiveFragment.java
@@ -9,6 +9,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.user.lessontracker.R;
+import com.example.user.lessontracker.database.LessonTrackerCursorWrapper;
+import com.example.user.lessontracker.database.LessonTrackerDbHelper;
 
 public class AddLearningObjectiveFragment extends Fragment {
 
@@ -25,7 +27,9 @@ public class AddLearningObjectiveFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_add_learning_objective, container, false);
 
+        String topicTitle = getArguments().getString("topicTitle");
         mTopicText = (TextView) view.findViewById(R.id.learning_objective_topic_text);
+        mTopicText.setText(topicTitle);
 
         mTitleEditText = (EditText) view.findViewById(R.id.learning_objective_title_edit);
         mDetailEditText = (EditText) view.findViewById(R.id.learning_objective_detail_edit);
