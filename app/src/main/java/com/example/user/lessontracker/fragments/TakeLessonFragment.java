12dd6f0377f468fcac0e246b982a363aa812316c
Bertie Croll <bert.croll@gmail.com>
2016-11-15 14:25:06 +0000

    fragment_take_lesson.xml and TakeLessonFragment created

---
+++ app/src/main/java/com/example/user/lessontracker/fragments/TakeLessonFragment.java
@@ -0,0 +1,14 @@
+package com.example.user.lessontracker.fragments;
+
+import android.support.v4.app.Fragment;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class TakeLessonFragment extends Fragment {
+
+    TextView mDetailsTextView;
+    ListView mLearningObjectiveList;
+
+
+
+}
