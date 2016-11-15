5c7ec7825bb3bcc640551dcbbf0127ceff96af42
Bertie Croll <bert.croll@gmail.com>
2016-11-15 16:44:12 +0000

    TaggingDialogFragment created

---
+++ app/src/main/java/com/example/user/lessontracker/fragments/TaggingDialogFragment.java
@@ -0,0 +1,18 @@
+package com.example.user.lessontracker.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class TaggingDialogFragment extends DialogFragment {
+
+    TextView mTaggingTitle;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup content, Bundle savedInstanceState) {
+        return null;
+    }
+}
