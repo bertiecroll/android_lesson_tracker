4540db78747fc76347562b9ba5df6da8fd3fd0e3
Bertie Croll <bert.croll@gmail.com>
2016-11-15 14:29:56 +0000

    onCreateView added to TakeLessonFragment

--- app/src/main/java/com/example/user/lessontracker/fragments/TakeLessonFragment.java
+++ app/src/main/java/com/example/user/lessontracker/fragments/TakeLessonFragment.java
@@ -1,14 +1,37 @@
 package com.example.user.lessontracker.fragments;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.example.user.lessontracker.R;
+import com.example.user.lessontracker.database.LessonTrackerDbHelper;
+
 public class TakeLessonFragment extends Fragment {
 
+    LessonTrackerDbHelper mDbhelper;
     TextView mDetailsTextView;
     ListView mLearningObjectiveList;
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_take_lesson, container, false);
+
+        mDbhelper = new LessonTrackerDbHelper(getActivity());
+
+        mDetailsTextView = (TextView) view.findViewById(R.id.take_lesson_details);
+        mLearningObjectiveList = (ListView) view.findViewById(R.id.take_lesson_learning_objectives);
+
+        return view;
+    }
 
 }
