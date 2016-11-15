485b7e161ab21b7fd5e9854b7d81977e2cdf0f07
Bertie Croll <bert.croll@gmail.com>
2016-11-15 18:53:54 +0000

    TaggingDialogFragment updated to add Tag buttons

--- app/src/main/java/com/example/user/lessontracker/fragments/TaggingDialogFragment.java
+++ app/src/main/java/com/example/user/lessontracker/fragments/TaggingDialogFragment.java
@@ -5,12 +5,21 @@ import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 import android.widget.TextView;
 
 import com.example.user.lessontracker.R;
+import com.example.user.lessontracker.database.LessonTrackerDbHelper;
+import com.example.user.lessontracker.models.Tag;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TaggingDialogFragment extends DialogFragment {
 
+    LessonTrackerDbHelper mDbHelper;
     TextView mTaggingTitle;
     TextView mLearningObjectiveTitle;
 
@@ -18,12 +27,25 @@ public class TaggingDialogFragment extends DialogFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_dialog_tagging, container, false);
 
+        mDbHelper = new LessonTrackerDbHelper(getActivity());
+        List<Tag> tags = new ArrayList<>(mDbHelper.allTags());
+        LinearLayout linearLayout = (LinearLayout) view.findViewById(R.id.tagging_dialog_fragment);
+        for (Tag tag : tags) {
+            Button tagButton = new Button(getActivity());
+            tagButton.setText(tag.getTitle());
+            linearLayout.addView(tagButton,
+                    new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+        }
+
+
         Bundle arguments = getArguments();
         String learningObjectiveTitle = arguments.getString("learningObjectiveTitle");
 
         mTaggingTitle = (TextView) view.findViewById(R.id.tagging_title);
         mLearningObjectiveTitle = (TextView) view.findViewById(R.id.tagging_learning_objective_title);
         mLearningObjectiveTitle.setText(learningObjectiveTitle);
+
+
 
         return view;
     }
