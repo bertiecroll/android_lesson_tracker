a2975f622412d19fbd62c28cf733a1d33f42613f
Bertie Croll <bert.croll@gmail.com>
2016-11-19 12:03:53 +0000

    TakeLessonActivity

--- app/src/main/java/com/example/user/lessontracker/activities/TakeLessonActivity.java
+++ app/src/main/java/com/example/user/lessontracker/activities/TakeLessonActivity.java
@@ -1,20 +1,21 @@
 package com.example.user.lessontracker.activities;
 
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 
 import com.example.user.lessontracker.R;
-import com.example.user.lessontracker.fragments.SubjectListFragment;
+import com.example.user.lessontracker.fragments.LessonListFragment;
 
-public class TeachableActivity extends LessonTrackerActivity {
+public class TakeLessonActivity extends LessonTrackerActivity {
 
     @Override
     protected void getFragment() {
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
         if (fragment == null) {
-            fragment = new SubjectListFragment();
+            fragment = new LessonListFragment();
             fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
         }
     }
